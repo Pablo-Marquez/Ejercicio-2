2e7979aabbd17a449a25afe53cc4e7d16237f4da
Pablo-Marquez <p.marquez.valiente@iescristobaldemonroy.es>
2023-03-24 09:45:06 +0100

    primer commit

---
+++ evaluacion2/src/Evaluacion2/TablaEnterosTest.java
@@ -0,0 +1,29 @@
+package Evaluacion2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TablaEnterosTest {
+
+	@BeforeEach
+	 void crearTabla() {
+		Integer[] tablaVacia;
+	}
+	
+	@Test
+	void testConstructorArrayVacio(Integer[] tablaVacia) {
+		
+		tablaVacia=null;
+		
+		
+		assertEquals(null, null);
+	}
+	@Test
+	void testSumaTabla() {
+		
+		
+		
+	}
+}
